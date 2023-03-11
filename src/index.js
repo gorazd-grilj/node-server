fbac10d7e8d3c3c72dc9aa4fdd39f3f694741831
Gorazd Grilj <gorazd.grilj@gmail.com>
2023-03-11 13:59:34 +0100

    dodani datoteki Dockerfile in docker-compose.yml

--- src/index.js
+++ src/index.js
@@ -3,6 +3,12 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import mongoose from "mongoose";
 
+const port = process.env.PORT || 5000;
+const mongoPort = process.env.MPORT || 27017;
+const mongoDB = process.env.MBD || "licila";
+const DB_URI = `mongodb://mongodb:${mongoPort}/${mongoDB}`; // docker
+// const DB_URI = `mongodb://localhost:${mongoPort}/${mongoDB}`;
+
 // mongo schema
 const todoSchema = new mongoose.Schema({
   name: { type: String, required: true },
@@ -12,7 +18,7 @@ const todoSchema = new mongoose.Schema({
   date_to: { type: Date, default: "" },
 });
 
-const todo = mongoose.model("ToDo", todoSchema);
+const ToDo = mongoose.model("ToDo", todoSchema);
 
 const server = express();
 server.use(bodyParser.json());
@@ -20,7 +26,7 @@ server.use(bodyParser.urlencoded({ extended: true }));
 server.use(
   cors({
     origin: "http://localhost:3000",
-    methods: "get,post",
+    methods: "GET,PUT,POST,DELETE",
   })
 );
 server.use(express.json());
@@ -30,38 +36,30 @@ const route = express.Router();
 // MIDDLEWARE
 server.use("/", async (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,POST");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
   res.header("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
 
-const port = process.env.PORT || 5000;
-const mongo = process.env.MONGO || 27017;
-
 server.use("/", route);
 
-route.get("/", (req, res) => {
-  res.status(200).send({ message: "root" });
+route.get("/", (_req, res) => {
+  res.status(200).send({ message: `Node server is up and running. ${new Date().toISOString()}` });
 });
 
 route.post("/add", async (req, res) => {
-  let result = await todo({
+  let result = await ToDo.create({
     name: req.body.user,
     task_name: req.body.task_name,
     description: req.body.description,
     date_from: req.body.date_from,
     date_to: req.body.date_to,
   });
-  result.save();
-  const cnt = await todo.find().count();
-  res
-    .status(201)
-    .send({ message: `Naloga št: ${cnt} je dodana.`, cnt: cnt });
+  res.status(201).send(result);
 });
 
 route.post("/list", async (req, res) => {
-  const result = await todo
-    .find()
+  const result = await ToDo.find()
     .skip(req.body.skip)
     .limit(req.body.limit)
     .sort({ date_from: -1 });
@@ -69,7 +67,7 @@ route.post("/list", async (req, res) => {
 });
 
 route.post("/find", async (req, res) => {
-  const result = await todo.find({
+  const result = await ToDo.find({
     $or: [
       { name: { $regex: req.body.find_todo } },
       { task_name: { $regex: req.body.find_todo } },
@@ -80,12 +78,12 @@ route.post("/find", async (req, res) => {
 });
 
 route.post("/id", async (req, res) => {
-  const result = await todo.findOne({ _id: req.body.list_todo });
+  const result = await ToDo.findOne({ _id: req.body.list_todo });
   res.status(200).send(result);
 });
 
-route.post("/update", async (req, res) => {
-  const result = await todo.updateOne(
+route.put("/update", async (req, res) => {
+  const result = await ToDo.updateOne(
     { _id: req.body._id },
     {
       $set: {
@@ -101,17 +99,17 @@ route.post("/update", async (req, res) => {
   res.status(200).send(result);
 });
 
-route.post("/delete", async (req, res) => {
-  const result = await todo.deleteOne({ _id: req.body._id });
+route.delete("/delete", async (req, res) => {
+  const result = await ToDo.deleteOne({ _id: req.body._id });
   res.status(200).send(result);
 });
 
 // connect to database
-mongoose.connect(`mongodb://localhost:${mongo}/licila`, {
+mongoose.connect(DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
 server.listen(port, () => {
-  console.log(`Server is listening on ${port}`);
+  console.log(`Node server is listening on ${port}`);
 });
